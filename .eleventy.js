ff149d8954d255cc4690a8da2e1530d072f9e409
Joe Cotellese <joe@cotellese.net>
2025-05-16 12:45:43 -0400

    refactor: Add CSS variables for improved theming and enhance jumbotron styling with hero image

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   // Add a global data value for GA4 Measurement ID, configurable via environment variable or fallback
   eleventyConfig.addGlobalData("ga4_id", process.env.GA4_ID || "G-EMPRP3KC10");
